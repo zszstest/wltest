5086889a281d42c6d1690ba623f2f580da02f9b6
zsagia <zsagia@gmail.com>
2020-10-24 13:21:55 +0200

    Create env as a config file

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const Sequelize = require('sequelize');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var Sequelize = require("sequelize");
+var config = require("./env.json")[process.env.NODE_ENV || "development"];
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-const sequelize = new Sequelize(
-  'postgres://postgres:mN09bV87cX@localhost:5432/postgres'
+var sequelize = new Sequelize(
+  "postgres://" +
+    config.DATABASE_USER_NAME_PASSWORD +
+    "@" +
+    config.DATABASE_URI +
+    "/" + config.DATABASE_NAME
 );
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('./dist/wltest'));
+app.use(express.static("./dist/wltest"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -32,21 +37,20 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 sequelize
   .authenticate()
-  .then(() => {
-    console.log('Connection of wltest app has been established successfully.');
+  .then(function () {
+    console.log("Connection of wltest app has been established successfully.", config);
   })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
+  .catch(function (err) {
+    console.error("Unable to connect to the database:", err);
   });
-
 
 module.exports = app;
